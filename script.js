1eddc39dbe980d74da6d5df39dc56206c8096f93
hchafla <161073227+hchafla@users.noreply.github.com>
2026-07-29 17:11:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
-.
+const API_URL = "https://script.googleusercontent.com/macros/echo?user_content_key=AUkAhnT8x8db9O6Ks6LahaeaAspbX4pBKDaomrBgBnM3H3noAdCmfF-lf46_uSSd0R9sp9bQYfh3qted2OL5mAEMJBaQzqz_09Hw0sB2xJaDT_F-5mA4QXH1z85OwMK4IF3AOCOLsx1mPR5ZVFLLnsFBxTzbWyAwagT3-e5WU88f0axajbfNBrWU2i1eVY9sIyUJ3WaEyLhLxaJO4dbKomm-zKh8O5xmG_YpmM1oZPPTJfaRpkNM2LW45yP9USL13_emsB6Xxmwws2XutMiFzCXWoxOC4JYf4Q&lib=MlJGY15a9XmDGY_xRk5gVQfkSPZuzewKU";
+
+let ejercicios = [];
+
+async function cargarEjercicios() {
+    try {
+        const respuesta = await fetch(API_URL);
+        ejercicios = await respuesta.json();
+
+        console.log("Ejercicios cargados:", ejercicios);
+
+        mostrarEjercicios(ejercicios);
+
+    } catch (error) {
+        console.error("Error cargando ejercicios:", error);
+    }
+}
+
+function mostrarEjercicios(lista) {
+
+    const contenedor = document.getElementById("biblioteca");
+
+    contenedor.innerHTML = "";
+
+    lista.forEach(ejercicio => {
+
+        contenedor.innerHTML += `
+            <div class="card">
+                <img src="${ejercicio.imagen}">
+                <h3>${ejercicio.nombre}</h3>
+                <p>${ejercicio.region}</p>
+            </div>
+        `;
+
+    });
+
+}
+
+window.onload = cargarEjercicios;
