4609c14b75b5fe8b95074875383105bb7b65270f
hchafla <161073227+hchafla@users.noreply.github.com>
2026-09-20 15:24:14 +0100

    Add multiple pathology entries to patologias.js

--- datos/fisio-ap/patologias.js
+++ datos/fisio-ap/patologias.js
@@ -3,5 +3,50 @@ window.FISIOAP_PATOLOGIAS = [
     "id": "hombro-doloroso",
     "nombre": "Hombro doloroso - Tendinopatías de hombro",
     "archivo": "hombro-doloroso.js"
+  },
+  {
+    "id": "epicondilalgias",
+    "nombre": "Codo - Epicondilalgias",
+    "archivo": "epicondilalgias.js"
+  },
+  {
+    "id": "gonartrosis",
+    "nombre": "Rodilla - Gonartrosis",
+    "archivo": "gonartrosis.js"
+  },
+  {
+    "id": "rodilla-otras",
+    "nombre": "Rodilla - Síndrome fémoro-patelar, tendinitis rotuliana y pata de ganso",
+    "archivo": "rodilla-otras.js"
+  },
+  {
+    "id": "coxartrosis",
+    "nombre": "Cadera - Coxartrosis",
+    "archivo": "coxartrosis.js"
+  },
+  {
+    "id": "esguince-tobillo",
+    "nombre": "Tobillo - Esguince de tobillo",
+    "archivo": "esguince-tobillo.js"
+  },
+  {
+    "id": "fascitis-plantar",
+    "nombre": "Pie - Fascitis plantar",
+    "archivo": "fascitis-plantar.js"
+  },
+  {
+    "id": "cervicalgia",
+    "nombre": "Algias vertebrales - Cervicalgia",
+    "archivo": "cervicalgia.js"
+  },
+  {
+    "id": "lumbalgia",
+    "nombre": "Algias vertebrales - Lumbalgia",
+    "archivo": "lumbalgia.js"
+  },
+  {
+    "id": "epoc",
+    "nombre": "EPOC",
+    "archivo": "epoc.js"
   }
 ];
